8f1bb188f25c855570e3790dc95820ed493dc887
MahiroCreative <127273095+MahiroCreative@users.noreply.github.com>
2026-03-24 16:58:54 +0900

    up

--- 04_Template/MyDxlib3DGameTemplate/MyDxlib/MyDxlib.h
+++ 04_Template/MyDxlib3DGameTemplate/MyDxlib/MyDxlib.h
@@ -1,6 +1,12 @@
 ﻿#pragma once
 #include "DxLib.h"
-//どれもDxlibにガッツリ依存しています。
-#include "MyDxlibDebug/MyDxlibDebug.h"
-#include "MyDxlibMath/MyDxlibMath.h"
-#include"MyDxlibGame/MyDxlibGame.h"
+//Math.
+#include "Vector2.h"
+#include "Vector3.h"
+#include "Quaternion.h"
+//Game.
+#include "_baseGameObject2D.h"
+#include "_baseGameScene.h"
+
+
+//Debug.
--- 04_Template/MyDxlib3DGameTemplate/MyDxlib/MyDxlibMath/MyDxlibMath.h
+++
@@ -1,7 +0,0 @@
-﻿#pragma once
-//Dxlib用の数学ライブラリ
-//自作ライブラリ
-#include "Vector3.h"
-#include "Quaternion.h"
-#include "Vector2.h"
-
